351c010ba9b3bd9fa4093176ecbfe83ee52e647f
anGie <angievmal17@gmail.com>
2025-09-30 18:07:05 -0500

    cambio4 linea 13

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ProyectoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("kiero makis");
+            Console.WriteLine("Bienvenidos a los juegos del hambre");
         }
     }
 }
