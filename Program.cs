c8f3785796dec9f96b95cffdae37d76133a17692
Danna Guarniz <dangenesisA09@outlook.com>
2025-09-30 18:25:07 -0500

    hice el cambio

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ProyectoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Actividad 06");
+            Console.WriteLine("Actividad 06_git");
         }
     }
 }
